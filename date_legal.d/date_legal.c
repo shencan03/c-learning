1939769eca9f2289b06d68ee0185e0bfd94b6157
shencan03 <shencan03@gmail.com>
2025-09-25 17:19:35 +0300

    prog

---
+++ date_legal.d/date_legal.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(void){
+  int month, day, year; 
+
+  printf("Enter date (mm/dd/yy): ");
+  scanf("%d/%d/%d", &month, &day, &year);
+
+  printf("Dated this ");
+  switch (day) {
+    case 1: case 11: case 21: printf("%dth", day); break;
+    case 2: case 12: case 22: printf("%dnd", day); break;
+    case 3: case 13: case 23: printf("%drd", day); break;
+    default: printf("%dth", day); break;
+  } 
+  printf(" day of ");
+
+  switch (month) {
+    case 1: printf("January"); break;
+    case 2: printf("February"); break;
+    case 3: printf("March"); break;
+    case 4: printf("April"); break;
+    case 5: printf("May"); break;
+    case 6: printf("June"); break;
+    case 7: printf("July"); break;
+    case 8: printf("August"); break;
+    case 9: printf("September"); break;
+    case 10: printf("October"); break;
+    case 11: printf("November"); break;
+    case 12: printf("December"); break;
+  }
+  /*Add day int with suffix switch-case*/
+  /*Add month printf*/
+  printf(", 20%d.", year);
+
+  return 0;
+}
