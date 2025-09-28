5ba10010dd1addac4e59ad54cabd6e9664468e10
shencan03 <shencan03@gmail.com>
2025-09-28 20:13:30 +0300

    prog

--- squares.d/squares.c
+++ squares.d/squares.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
 int main(void) {
-  int n, i;
+  int n;
 
   printf("This program prints a table of squares.\nEnter number of entries in table: ");
   scanf("%d", &n);
+  getchar();
 
-  i = 1;
-  while (n >= i){
-    printf("%10d%10d\n", i, i * i);
-    i++;
-  }
-    
+  for (int i = 1; i <= n; i++){
+    printf("%10d%10ld\n", i, (long) i * i);
+    if ((i % 24) == 0){
+      printf("Press Enter to continue.. ");
+      getchar();
+    }
+  } 
+
   return 0;
 }
