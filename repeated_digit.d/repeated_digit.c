0dfeab97c7b37598884933ddb534f2c2cde7b5de
shencan03 <shencan03@gmail.com>
2025-10-02 19:23:24 +0300

    prog

--- repeated_digit.d/repeated_digit.c
+++ repeated_digit.d/repeated_digit.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
-#include <stdbool.h>
 
 int main(void){
-  long n;
-  int i[10] = {false};
+  long num;
+  int digits[10] = {0};
   int digit;
 
   printf("Enter a number: ");
-  scanf("%ld", &n);
+  scanf("%ld", &num);
 
-  while (n > 0){
-    digit = n % 10;
-    if (i[digit] == 1){
-      break;
+  digit = num % 10;
+  while (num != 0){
+    digits[digit]++;
+    num /= 10;
+    digit = num % 10;
+  }
+
+  printf("Digit:");
+  printf("%6c", ' ');
+  for (int i = 0; i < (unsigned) (sizeof(digits) / sizeof(digits[0])); i++){
+      printf("%3d", i);
     }
-    i[digit] = true;
-    n /= 10;
-  }
 
-  if (n > 0){
-    printf("Repeated digit");
-  } else {
-    printf("No repeated digit");
-  }
-  
+  printf("\nOccurences: ");
+  for (int i = 0; i < (unsigned) (sizeof(digits) / sizeof(digits[0])); i++){
+      printf("%3ld", digits[i]);
+    }
+
   return 0;
 }
