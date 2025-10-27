647b27ad5272e4b31ecd768acddaf5a7b54217b1
shencan03 <shencan03@gmail.com>
2025-10-27 22:34:33 +0300

    functions and scopes

---
+++ higer_lower.d/higher_lower.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_NUMBER 100
+
+int new_secret_number(void);
+void initialize_number_generator(void);
+void read_guesses(int secret_number);
+
+int main(void) {
+  char command;
+  int secret_number;
+
+  printf("Guess the secret number between 1 and %d.\n\n", MAX_NUMBER);
+  initialize_number_generator();
+  do {
+    secret_number = new_secret_number();
+    printf("A new number has been chosen.\n");
+    read_guesses(secret_number);
+    printf("Play again? (Y/N): ");
+    scanf(" %c", &command);
+    printf("\n");
+  } while (command == 'Y' || command == 'y');
+
+  return 0;
+}
+
+void initialize_number_generator(void) { srand((unsigned)time(NULL)); }
+
+int new_secret_number(void) { return rand() % MAX_NUMBER + 1; }
+
+void read_guesses(int secret_number) {
+  int guess, count;
+
+  for (;;) {
+    count++;
+    printf("Enter guess: ");
+    scanf("%d", &guess);
+    if (guess == secret_number) {
+      printf("%d is correct!\nYou guessed in %d attempts.\n\n", secret_number,
+             count);
+      return;
+    } else if (guess < secret_number) {
+      printf("%d is too low! Try again.\n", guess);
+    } else {
+      printf("%d is too high! Try again.\n", guess);
+    }
+  }
+}
