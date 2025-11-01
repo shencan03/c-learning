d2bd7808a9afdb471a0df9b0653f5ff646f1333f
shencan03 <shencan03@gmail.com>
2025-11-02 01:59:27 +0300

    fix: move func prototype below main

--- min_max.d/min_max.c
+++ min_max.d/min_max.c
@@ -1,19 +1,7 @@
 #include <stdio.h>
 #define N 10
 
-void min_max(int i[], int *smallest, int *largest) {
-  *smallest = i[0], *largest = i[0];
-
-  for (int x = 1; x < N; x++) {
-    if (i[x] > *largest) {
-      *largest = i[x];
-      continue;
-    }
-    if (i[x] < *smallest) {
-      *smallest = i[x];
-    }
-  }
-}
+void min_max(int i[], int *smallest, int *largest);
 
 int main(void) {
   int i[N], smallest, largest;
@@ -28,4 +16,18 @@ int main(void) {
   printf("Smallest: %d\nLargest: %d", smallest, largest);
 
   return 0;
+}
+
+void min_max(int i[], int *smallest, int *largest) {
+  *smallest = i[0], *largest = i[0];
+
+  for (int x = 1; x < N; x++) {
+    if (i[x] > *largest) {
+      *largest = i[x];
+      continue;
+    }
+    if (i[x] < *smallest) {
+      *smallest = i[x];
+    }
+  }
 }
