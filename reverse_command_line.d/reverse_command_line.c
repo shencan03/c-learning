f63df663d40576660cc9e402216e14833ee13634
shencan03 <shencan03@gmail.com>
2025-11-17 21:22:16 +0300

    prog

--- reverse_command_line.d/reverse_command_line.c
+++ reverse_command_line.d/reverse_command_line.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  int i;
+  int i, total = 0;
 
-  for (i = argc - 1; 1 <= i; i--) {
-    printf("%s ", argv[i]);
+  for (i = 1; i < argc; i++) {
+    total += atoi(argv[i]);
   }
 
+  printf("Total: %d", total);
   return 0;
 }
